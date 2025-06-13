d22bddda108680212691169112953c2433c5b2b9
Anton Gorokh <anton.gorokh@princity.com>
2025-06-13 18:15:00 +0200

    some backend shi

--- backend/src/test/java/com/calculatorify/model/repository/AbstractTest.java
+++ backend/src/test/java/com/calculatorify/model/repository/AbstractTest.java
@@ -5,7 +5,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.MockedStatic;
@@ -28,40 +27,33 @@ public abstract class AbstractTest {
 			.withPassword("test")
 			.withReuse(true);
 
-	static {
-		CONTAINER_POSTGRES.start();
-	}
-
 	protected static final UUID TEST_UUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
 
-	protected HikariDataSource dataSource;
-	protected MockedStatic<TransactionContext> txSpy;
+   protected static HikariDataSource dataSource;
+   protected static MockedStatic<TransactionContext> txSpy;
 
-	@BeforeAll
-	public void beforeAll() {
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl(CONTAINER_POSTGRES.getJdbcUrl());
-		config.setUsername(CONTAINER_POSTGRES.getUsername());
-		config.setPassword(CONTAINER_POSTGRES.getPassword());
-		config.setDriverClassName("org.postgresql.Driver");
+   // Initialize container, database schema, and transaction context once per test class
+   static {
+       CONTAINER_POSTGRES.start();
+       HikariConfig config = new HikariConfig();
+       config.setJdbcUrl(CONTAINER_POSTGRES.getJdbcUrl());
+       config.setUsername(CONTAINER_POSTGRES.getUsername());
+       config.setPassword(CONTAINER_POSTGRES.getPassword());
+       config.setDriverClassName("org.postgresql.Driver");
+       try (HikariDataSource migrationDs = new HikariDataSource(config)) {
+           Flyway.configure()
+                 .dataSource(migrationDs)
+                 .locations("classpath:db/migration")
+                 .load()
+                 .migrate();
+       }
+       dataSource = new HikariDataSource(config);
+       TransactionContext.setDataSource(dataSource);
+       txSpy = mockStatic(TransactionContext.class, CALLS_REAL_METHODS);
+       txSpy.when(() -> TransactionContext.inTransaction(any(Callable.class)))
+            .thenAnswer(args -> args.getArgument(0, Callable.class).call());
+   }
 
-		try (HikariDataSource migrationDs = new HikariDataSource(config)) {
-			Flyway.configure()
-					.dataSource(migrationDs)
-					.locations("classpath:db/migration")
-					.load()
-					.migrate();
-		}
-		dataSource = new HikariDataSource(config);
-		TransactionContext.setDataSource(dataSource);
-		txSpy = mockStatic(TransactionContext.class, CALLS_REAL_METHODS);
-
-		// wrap the whole test with a transaction that will be rolled back after each test
-		txSpy.when(() -> TransactionContext.inTransaction(any(Callable.class)))
-				.thenAnswer(args -> {
-					return args.getArgument(0, Callable.class).call();
-				});
-	}
 
 	@BeforeEach
 	public void beforeEach() {
@@ -75,15 +67,5 @@ public abstract class AbstractTest {
 		}
 		TransactionContext.rollback();
 		TransactionContext.end();
-	}
-
-	@AfterAll
-	public void afterAll() {
-		if (dataSource != null) {
-			dataSource.close();
-		}
-		if (CONTAINER_POSTGRES != null) {
-			CONTAINER_POSTGRES.stop();
-		}
 	}
 }
